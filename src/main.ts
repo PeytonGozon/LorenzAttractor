bea6e4ee72342bfb91cb87a2b26d5edf090ab7e7
Peyton Gozon <peyton.gozon@gmail.com>
2023-03-25 18:56:37 -0500

    v1.0

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import * as THREE from 'three'
-import { Float32BufferAttribute } from 'three'
+import { BufferAttribute, Float32BufferAttribute } from 'three'
 import { LorenzAttractor } from './lorenz-attractor'
 import './style.css'
 
 class App {
-  static MAX_POINTS = 100_000
-
   private scene: THREE.Scene
   private camera: THREE.PerspectiveCamera
   private renderer: THREE.WebGLRenderer
@@ -16,10 +14,15 @@ class App {
 
   private numCurrentPoints: number
 
-  constructor(private speed: number) {
+  constructor(private max_points: number, private speed: number) {
+    const container = document.getElementById('container')
+
     this.scene = new THREE.Scene()
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500)
     this.renderer = new THREE.WebGLRenderer()
+
+    container?.appendChild(this.renderer.domElement)
+    // document.body.appendChild(this.renderer.domElement)
 
     this.setupCamera()
     this.setupRenderer()
@@ -28,19 +31,40 @@ class App {
     this.numCurrentPoints = 0
 
     // 3 floats to each point: x, y, z
-    const positions = new Float32Array(App.MAX_POINTS * 3)
+    const positions = new Float32Array(this.max_points * 3)
     const geometry = new THREE.BufferGeometry()
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
 
-    const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
+    // https://www.wayneparrott.com/how-to-create-three-js-gradient-colored-lines-and-circlelines-part-2/
+    // const colors = this.setupColors()
+    // geometry.setAttribute('color', new BufferAttribute(colors, 3))
+
+    // const material = new THREE.LineBasicMaterial({
+    //   vertexColors: true
+    // })
+
+    const material = new THREE.LineBasicMaterial({
+      color: "#0000ff"
+    })
 
     this.line = new THREE.Line(geometry, material)
 
     this.scene.add(this.line)
   }
 
+  setupColors(): Float32Array {
+    const colorArr = Array(this.max_points).fill(0).flatMap((_, index) => {
+      const hue = 360.0 * index / this.max_points
+      const color = new THREE.Color().setHSL(hue, 1, 0.5)
+
+      return [color.r, color.b, color.g]
+    })
+
+    return new Float32Array(colorArr)
+  }
+
   setupCamera() {
-    this.camera.position.set(0, 0, 100)
+    this.camera.position.set(0, 0, 80)
     this.camera.lookAt(0, 0, 0)
   }
 
@@ -48,8 +72,6 @@ class App {
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     this.renderer.outputEncoding = THREE.sRGBEncoding
-
-    document.body.appendChild(this.renderer.domElement)
   }
 
   onWindowResize() {
@@ -63,7 +85,7 @@ class App {
     let addedAtLeastOnePoint = false
 
     for (let i = 0; i < this.speed; i++) {
-      if (this.numCurrentPoints < App.MAX_POINTS) {
+      if (this.numCurrentPoints < this.max_points) {
         const point = this.lorenzAttractor.nextPoint()
 
         const positions = this.line.geometry.attributes.position as Float32BufferAttribute
@@ -87,12 +109,15 @@ class App {
 
     this.renderer.render(this.scene, this.camera)
   }
+
+  set setSpeed(speed: number) {
+    this.speed = speed
+  }
 }
 
-const lorenzAttractorApp = new App(1)
-console.log(lorenzAttractorApp)
+const lorenzAttractorApp = new App(100_000, 10)
 
-window.addEventListener('resize', lorenzAttractorApp.onWindowResize)
+window.addEventListener('resize', () => lorenzAttractorApp.onWindowResize())
 
 function animate() {
   requestAnimationFrame(animate)
